f679fc867d5901a52cf7dfa128c2d365bba1a4d1
davstromb <davstromb@gmail.com>
2017-12-08 20:00:28 +0100

    JavaVariable

--- src/main/java/com/github/davstromb/shell4j/model/JavaVariable.java
+++ src/main/java/com/github/davstromb/shell4j/model/JavaVariable.java
@@ -3,6 +3,10 @@ package com.github.davstromb.shell4j.model;
 public class JavaVariable implements Code {
 
 
+    public JavaVariable(String variableAsString) {
+
+    }
+
     @Override
     public String code() {
         return "";
