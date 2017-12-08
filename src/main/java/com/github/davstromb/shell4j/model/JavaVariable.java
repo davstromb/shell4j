0609c3671c67d7f346996e2f4877344de1742990
davstromb <davstromb@gmail.com>
2017-12-08 20:02:42 +0100

    Variable eq variable lol

--- src/main/java/com/github/davstromb/shell4j/model/JavaVariable.java
+++ src/main/java/com/github/davstromb/shell4j/model/JavaVariable.java
@@ -3,12 +3,14 @@ package com.github.davstromb.shell4j.model;
 public class JavaVariable implements Code {
 
 
-    public JavaVariable(String variableAsString) {
+    private final String variableAsString;
 
+    public JavaVariable(String variableAsString) {
+        this.variableAsString = variableAsString + "=" + variableAsString + ";";
     }
 
     @Override
     public String code() {
-        return "";
+        return this.variableAsString;
     }
 }
