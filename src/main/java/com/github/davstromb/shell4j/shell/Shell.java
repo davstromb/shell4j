ecc3646062027642b464d6579440b8189cf3cf2a
Edward Grippe <edward.grippe@gmail.com>
2017-12-08 15:51:17 +0100

    added multiline input to shell

--- src/main/java/com/github/davstromb/shell4j/shell/Shell.java
+++ src/main/java/com/github/davstromb/shell4j/shell/Shell.java
@@ -1,5 +1,9 @@
 package com.github.davstromb.shell4j.shell;
 
+import com.github.davstromb.shell4j.execute.Executor;
+import com.github.davstromb.shell4j.execute.JavaExecutor;
+import com.github.davstromb.shell4j.model.JavaCode;
+
 import java.io.Console;
 import java.util.Scanner;
 
@@ -7,7 +11,10 @@ public class Shell {
     private final boolean verbose;
     private final boolean importEverything;
 
-    private final Scanner scanner;
+    private final char CMD_CHAR = ':';
+
+    private final Scanner  scanner  = new Scanner(System.in);
+    private final Executor executor = new JavaExecutor();
 
     public Shell() {
         this(false, true);
@@ -20,31 +27,68 @@ public class Shell {
     public Shell(boolean verbose, boolean importEverything) {
         this.verbose = verbose;
         this.importEverything = importEverything;
-
-        scanner = new Scanner(System.in);
     }
 
     public void run() {
-        while (true) {
+        StringBuilder sb = new StringBuilder();
+        printPrompt();
+        shell_loop: while (true) {
             String input = readInput();
-            if (isExitCommand(input)) {
-                break;
+
+            if (isCommand(input)) {
+                switch (input.substring(1)) {
+                    case "save":
+                        save();
+                        break;
+                    case "quit":
+                        break shell_loop;
+                    default:
+                        System.out.println("Command (" + input + ") not recognized");
+                }
+                printPrompt();
+            } else {
+                sb.append(input);
+                if (isFinishedStatement(sb)) {
+                    System.out.println(sb);
+
+                    JavaCode code = new JavaCode(sb.toString());
+                    executor.append(code);
+
+                    sb = new StringBuilder();
+                    printPrompt();
+                } else {
+                    sb.append(" ");
+                    printContinuationPrompt();
+                }
             }
-            System.out.println(input);
         }
+
+        System.out.println("Goodbye!");
     }
 
-    private boolean isExitCommand(String input) {
-        return input.equals("/quit");
+    private void printContinuationPrompt() {
+        System.out.print("... ");
+    }
+
+    private boolean isFinishedStatement(StringBuilder sb) {
+        return sb.toString().endsWith(";");
     }
 
     private String readInput() {
-        printPrompt();
         return scanner.nextLine();
     }
 
     private void printPrompt() {
-        System.out.print(">");
+        System.out.print("<>> ");
+    }
+
+    private void save() {
+        // TODO: 2017-12-08
+        System.out.println("Saving session to file...");
+    }
+
+    private boolean isCommand(String input) {
+        return input.startsWith(String.valueOf(CMD_CHAR));
     }
 
     public static void main(String[] args) {
