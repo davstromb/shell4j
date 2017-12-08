c9a4de28fe3097640836740cfa7f55969c58f098
Hassan Shah <hassanshah7866@users.noreply.github.com>
2017-12-08 14:43:23 +0100

    main application created and pom jar config done

--- src/main/java/com/github/davstromb/shell4j/Shell4jApplication.java
+++ src/main/java/com/github/davstromb/shell4j/Shell4jApplication.java
@@ -1,5 +1,6 @@
 package com.github.davstromb.shell4j;
 
+import com.github.davstromb.shell4j.shell.Shell;
 import org.apache.commons.cli.*;
 
 public class Shell4jApplication {
@@ -7,11 +8,13 @@ public class Shell4jApplication {
 
     public static void main(String... args) {
         Options options = new Options();
-        options.addOption("Verbose", "More details in logging. Disabled as default.");
-        options.addOption("Import", "Enables import of external classes. Disabled as default.");
+        options.addOption("verbose", "More details in logging. Disabled as default.");
+        options.addOption("import",  "Enables import of external classes. Disabled as default.");
         CommandLineParser parser = new DefaultParser();
         try {
             CommandLine cmd = parser.parse(options, args);
+            Shell shell = new Shell(cmd.hasOption("verbose"), cmd.hasOption("import"));
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
