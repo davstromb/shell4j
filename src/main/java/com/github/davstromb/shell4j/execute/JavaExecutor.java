7478f08cb0ca6b1e76d868882715543de0c0ef43
davstromb <davstromb@gmail.com>
2017-12-08 13:44:24 +0100

    Executor

---
+++ src/main/java/com/github/davstromb/shell4j/execute/JavaExecutor.java
@@ -0,0 +1,22 @@
+package com.github.davstromb.shell4j.execute;
+
+import com.github.davstromb.shell4j.model.JavaCode;
+
+public class JavaExecutor implements Executor {
+
+    public void clean() {
+
+    }
+
+    public void addCode(JavaCode code) {
+
+    }
+
+    public String execute() {
+        return null;
+    }
+
+    public void print() {
+
+    }
+}
