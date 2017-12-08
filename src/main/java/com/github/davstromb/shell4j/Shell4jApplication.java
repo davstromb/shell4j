d0f20e6119dca282160b24463c3e1b226ee7e7ea
Hassan Shah <hassanshah7866@users.noreply.github.com>
2017-12-08 14:18:12 +0100

    added options and parser to Shell4jApplication

--- src/main/java/com/github/davstromb/shell4j/Shell4jApplication.java
+++ src/main/java/com/github/davstromb/shell4j/Shell4jApplication.java
@@ -1,10 +1,20 @@
 package com.github.davstromb.shell4j;
 
+import org.apache.commons.cli.*;
+
 public class Shell4jApplication {
 
 
     public static void main(String... args) {
-
+        Options options = new Options();
+        options.addOption("Verbose", "More details in logging. Disabled as default.");
+        options.addOption("Import", "Enables import of external classes. Disabled as default.");
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }
