e6c4eefa583cba67f05509af495fdd169b5a7faa
davstromb <davstromb@gmail.com>
2017-12-08 14:16:53 +0100

    Executor and executing test

--- src/main/java/com/github/davstromb/shell4j/execute/JavaExecutor.java
+++ src/main/java/com/github/davstromb/shell4j/execute/JavaExecutor.java
@@ -3,13 +3,30 @@ package com.github.davstromb.shell4j.execute;
 import com.github.davstromb.shell4j.execute.print.Printer;
 import com.github.davstromb.shell4j.model.JavaCode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class JavaExecutor implements Executor {
 
 
+    public static final String JAVA_BASE_TXT = "/JavaBase.txt";
     private StringBuilder cache;
 
     public JavaExecutor() {
-        this.cache = new StringBuilder();
+        try {
+            this.cache = new StringBuilder(new String(
+                    Files.readAllBytes(
+                            Paths.get(getClass().getResource(JAVA_BASE_TXT).toURI())))
+            );
+        } catch(Exception e) {
+            throw new ExecutionException("Can not execute", e);
+        }
+
     }
 
     public void clean() {
---
+++ src/test/java/com/github/davstromb/shell4j/execute/TestJavaExecutor.java
@@ -0,0 +1,7 @@
+package com.github.davstromb.shell4j.execute;
+
+public class TestJavaExecutor {
+
+
+
+}
