46a2970d7619212bf7e81ef298cb21c8e9773ab6
davstromb <davstromb@gmail.com>
2017-12-08 13:49:16 +0100

    printer

---
+++ src/main/java/com/github/davstromb/shell4j/execute/print/Printer.java
@@ -0,0 +1,13 @@
+package com.github.davstromb.shell4j.execute.print;
+
+public class Printer {
+
+
+    public static Printer create() {
+        return new Printer();
+    }
+
+    public void save(StringBuilder cache) {
+
+    }
+}
